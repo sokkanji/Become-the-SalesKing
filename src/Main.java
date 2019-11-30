224c5084ff51dd35f04025c2f134bb9c38b5d468
kkanji <s2018s07@e-mirim.hs.kr>
2019-11-30 15:39:02 +0900

    배경음악 추가

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-	public static final int SCREEN_WIDTH=1344;
-	public static final int SCREEN_HEIGHT=780;
-
-	public static void main(String ar[]) throws InterruptedException {
-		new Intro();
-	}
-}
---
+++ src/POS/Manage.java
@@ -0,0 +1,14 @@
+package POS;
+
+import javax.swing.*;
+
+class Manage extends JFrame{
+	JPanel panel = new JPanel();
+	
+	Manage(){
+		add(panel);
+		setSize(1000,500); 
+        setResizable(false);
+        setVisible(true);
+	}
+}
