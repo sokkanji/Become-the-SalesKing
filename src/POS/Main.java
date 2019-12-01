264e7b92ae9f4cf4948c90ab8b1034ec6063d766
Kimyen0426 <s2018s01@e-mirim.hs.kr>
2019-12-01 14:59:50 +0900

    포스기, 재고관리 db 완료

--- src/POS/Main.java
+++ src/POS/Main.java
@@ -3,6 +3,7 @@ package POS;
 public class Main {
 	public static final int SCREEN_WIDTH=1344;
 	public static final int SCREEN_HEIGHT=780;
+	static final int MONEY = 100000;
 
 	public static void main(String ar[]) throws InterruptedException {
 		new Intro();
